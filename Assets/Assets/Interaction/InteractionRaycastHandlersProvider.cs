80feb7266d7244b7a87e2db139ef43c7b0d2ae99
Mateusz Serkies <mserkies@artifexmundi.com>
2019-03-19 18:41:58 +0100

    WIP on interaction system.

---
+++ Assets/Assets/Interaction/InteractionRaycastHandlersProvider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public sealed class InteractionRaycastHandlersProvider : InteractionHandlersProvider
+{
+    [SerializeField]
+    private float m_raycastDistance = 10.0f;
+
+    [SerializeField]
+    private Transform m_originTransform = null;
+
+    public override InteractionHandler ProvideHandler()
+    {
+        Debug.DrawRay(m_originTransform.position, m_originTransform.forward * m_raycastDistance);
+
+        RaycastHit raycastResult;
+        if (!Physics.Raycast(m_originTransform.position, m_originTransform.forward, out raycastResult, m_raycastDistance))
+            return null;
+
+        return raycastResult.transform.GetComponent<InteractionHandler>();
+    }
+}
