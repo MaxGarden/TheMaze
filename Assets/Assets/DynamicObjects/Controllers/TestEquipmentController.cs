b9db91e1fd9a4caf3f5b625cfe5b48c570898609
Mateusz Serkies <mserkies@artifexmundi.com>
2019-03-30 19:52:21 +0100

    Added test equipment.

---
+++ Assets/Assets/DynamicObjects/Controllers/TestEquipmentController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public sealed class TestEquipmentController : UtilityEquipmentController
+{
+    public override void OnUse()
+    {
+        Equipment.Durability -= Equipment.Template.UseCost;
+
+        if(Equipment.Durability < 0.0f)
+        {
+            Equipment.Durability = 0.0f;
+            Debug.Log("Nope");
+        }
+        else
+            Debug.Log($"Use: {Equipment.Durability} / {Equipment.Template.Durability} ");
+    }
+}
