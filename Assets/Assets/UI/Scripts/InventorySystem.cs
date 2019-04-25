da47c4e2d35196d977bfd5e837e03f2ab22f3423
olamusial <olaa12@gmail.com>
2019-04-25 22:14:36 +0200

    Showing sprites while adding equipment - working on inventory system

---
+++ Assets/Assets/UI/Scripts/InventorySystem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySystem : MonoBehaviour
+{
+    public const int numberItemSlots = 4;
+    public Equipment[] items = new Equipment[numberItemSlots];
+    public Image[] itemImages = new Image[numberItemSlots];
+    private Inventory inventory;
+
+    private void OnEnable()
+    {
+        inventory = PlayerContext.MainPlayer.Inventory;
+        inventory.OnEquipmentChanged += EquipmentChange;
+
+    }
+
+    private void OnDisable()
+    {
+        inventory.OnEquipmentChanged -= EquipmentChange;
+    }
+
+    void EquipmentChange()
+    {
+        
+        for (int i = 0; i < items.Length; i++)
+        {   
+            if(items[i] == inventory.SelectedEquipment)           
+                return;
+            
+
+            if (items[i] == null)
+            {
+                items[i] = inventory?.SelectedEquipment;
+                itemImages[i].sprite = inventory?.SelectedEquipment.Template.Icon;
+                itemImages[i].enabled = true;
+                return;
+            }
+        }
+        
+    }
+    
+}
