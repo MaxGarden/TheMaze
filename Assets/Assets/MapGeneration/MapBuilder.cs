d4ac621e353ace47583a7a3624b96df1ef2ae4c1
karo1404 <karol1404@vp.pl>
2019-05-03 19:02:04 +0200

    Normalized existing map elements to grid

--- Assets/Assets/MapGeneration/MapBuilder.cs
+++ Assets/Assets/MapGeneration/MapBuilder.cs
@@ -9,15 +9,16 @@ public class MapBuilder : MonoBehaviour
 
     private float gridSize = 5.9f;
     private float scaleMargin = 0.1f;
-    private Vector3 positionOffset = new Vector3(0.0f, 0.0f, 25.0f);
+    private Vector3 position = new Vector3(0.0f, 0.0f, 25.0f);
 
     private string prefabsPath = "Decrepit Dungeon LITE/Prefabs";
 
 
     //notation: (R: element type, G: element id, B: height level, Alpha: rotation)
-    private (byte type, byte id, byte height, byte rotation)[] map = {(0,0,0,255),(0,20,0,255),(0,40,0,255),(0,100,0,255),(255,255,255,255),(0,60,0,255),(0,80,0,255),(0,0,0,255),(0,60,0,255),
-    (0,0,1,223),(0,20,1,223),(0,40,1,223),(0,100,1,223),(255,255,255,255),(0,60,1,223),(0,80,1,223),(0,0,1,223),(0,60,1,223),
-    (0,0,2,191),(0,20,2,191),(0,40,2,191),(0,100,2,191),(255,255,255,255),(0,60,2,191),(0,80,2,191),(0,0,2,191),(0,60,2,191)};
+    private (byte type, byte id, byte height, byte rotation)[] map = {
+    (0,0,0,255),(0,20,0,255),(0,40,0,255),(0,100,0,255),(0,0,0,223),(0,60,0,255),(0,80,0,255),(0,0,0,255),(0,60,0,255),
+    (0,0,1,223),(0,20,1,223),(0,40,1,223),(0,100,1,223),(0,0,1,223),(0,60,1,223),(0,80,1,223),(0,0,1,223),(0,60,1,223),
+    (0,0,2,159),(0,20,2,159),(0,40,2,159),(0,100,2,159),(0,0,2,159),(0,60,2,159),(0,80,2,159),(0,0,2,159),(0,60,2,159)};
 
     public void generate()
     {
@@ -34,6 +35,7 @@ public class MapBuilder : MonoBehaviour
             {
                 int pixelNumber = y * mapWidth + x;
                 Vector3 pos = new Vector3(x, map[pixelNumber].height, y) * gridSize;
+                Vector3 posOffset = new Vector3(0, 0, 0);
 
                 GameObject spawner = null;
                 Vector3 scale = new Vector3(gridSize, gridSize, gridSize);
@@ -51,19 +53,28 @@ public class MapBuilder : MonoBehaviour
                                 break;
                             case 20:
                                 elementPath += "/Wall_B";
+                                posOffset.x += -0.51f;
                                 break;
                             case 40:
                                 elementPath += "/Wall_C";
+                                scale.x += 4 * scaleMargin;
                                 break;
                             case 60:
                                 elementPath += "/Wall_D";
+                                scale.x += 2 * scaleMargin;
+                                posOffset.x += -0.35f;
                                 break;
                             case 80:
                                 elementPath += "/Wall_E";
+                                scale.x += 3.5f * scaleMargin;
+                                scale.z += scaleMargin;
+                                posOffset.x += 1.11f;
                                 break;
                             case 100:
                                 elementPath = prefabsPath + "/Arches/Arch_A";
+                                scale.x += 8 * scaleMargin;
                                 scale.z += scaleMargin;
+                                posOffset.x += 0.05f;
                                 break;
                         }
                         break;
@@ -74,7 +85,7 @@ public class MapBuilder : MonoBehaviour
                         switch (map[pixelNumber].id)
                         {
                             case 0:
-                                
+
                                 break;
                         }
                         break;
@@ -85,7 +96,7 @@ public class MapBuilder : MonoBehaviour
                         switch (map[pixelNumber].id)
                         {
                             case 0:
-                                
+
                                 break;
                         }
                         break;
@@ -96,7 +107,7 @@ public class MapBuilder : MonoBehaviour
                         switch (map[pixelNumber].id)
                         {
                             case 0:
-                                
+
                                 break;
                         }
                         break;
@@ -107,14 +118,14 @@ public class MapBuilder : MonoBehaviour
                         switch (map[pixelNumber].id)
                         {
                             case 0:
-                                
+
                                 break;
                         }
                         break;
                 }
 
-                if(Resources.Load(elementPath))
-                { 
+                if (Resources.Load(elementPath))
+                {
                     spawner = Instantiate((GameObject)Resources.Load(elementPath, typeof(GameObject)), pos, Quaternion.identity, parentObject.transform);
                 }
 
@@ -122,11 +133,12 @@ public class MapBuilder : MonoBehaviour
                 {
                     rescale(spawner, scale.x, scale.y, scale.z);
                     spawner.transform.Rotate(0, 90f * ((255 - map[pixelNumber].rotation) / 32), 0, Space.World);
+                    spawner.transform.Translate(posOffset, Space.Self);
                 }
 
             }
         }
-        parentObject.transform.position = positionOffset;
+        parentObject.transform.position = position;
     }
 
     public void rescale(GameObject gameObj, float newSizeX, float newSizeY, float newSizeZ)
