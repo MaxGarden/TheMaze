ac294913f3596081bc63556fa21100b64a18c061
Mateusz Serkies <mserkies@artifexmundi.com>
2019-05-13 18:53:04 +0200

    Added headers to templates.

--- Assets/Assets/DynamicObjects/Templates/CollectibleTemplate.cs
+++ Assets/Assets/DynamicObjects/Templates/CollectibleTemplate.cs
@@ -3,6 +3,8 @@
 [CreateAssetMenu(fileName = "CollectibleTemplate", menuName = "Templates/Collectible Template")]
 public sealed class CollectibleTemplate : InventoryObjectTemplate
 {
+    [Header("Collectible")]
+
     [SerializeField]
     private int m_collectIncrement = 1;
     public int CollectIncrement => m_collectIncrement;
