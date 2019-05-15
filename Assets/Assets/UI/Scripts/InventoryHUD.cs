68ea866a1f0249dfc5f3068263531fdadc837cb0
olamusial <olaa12@gmail.com>
2019-05-15 14:53:13 +0200

    Inventory HUD - added frame to selected item

--- Assets/Assets/UI/Scripts/InventoryHUD.cs
+++ Assets/Assets/UI/Scripts/InventoryHUD.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class InventorySystem : MonoBehaviour
+public class InventoryHUD : MonoBehaviour
 {
     public const int numberItemSlots = 4;
     public Equipment[] items = new Equipment[numberItemSlots];
     public Image[] itemImages = new Image[numberItemSlots];
+    public Image[] selectedImages = new Image[numberItemSlots];
     private Inventory inventory;
 
     private void OnEnable()
     {
         inventory = PlayerContext.MainPlayer.Inventory;
         inventory.OnEquipmentChanged += EquipmentChange;
-
     }
 
     private void OnDisable()
@@ -29,6 +29,20 @@ public class InventorySystem : MonoBehaviour
 
             itemImages[index].sprite = equipment ? equipment.Template.Icon : null;
             itemImages[index].enabled = !!equipment;
+            items[index] = equipment;
+
+             if(inventory.SelectedEquipment == entry.Value)
+             {
+                 selectedImages[index].enabled = true;
+             } else
+             {
+                 selectedImages[index].enabled = false;
+             }
+
+             if(selectedImages[index].enabled && !itemImages[index].enabled)
+             {
+                 selectedImages[index].enabled = false;
+             }
         }
     }
 }
