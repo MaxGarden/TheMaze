9cbcc6bbdc13a8a377acf7627d46af0ffa9474c3
Jakub Barczyk <jakubar632@student.polsl.pl>
2019-06-28 15:07:54 +0200

    Added door.

---
+++ Assets/Assets/DynamicObjects/Templates/DoorTemplate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DoorTemplate", menuName = "Templates/Door Template")]
+public sealed class DoorTemplate : InteractiveObjectTemplate
+{
+    [Header("Door")]
+
+    [SerializeField]
+    private AudioClip m_openSound = null;
+    public AudioClip OpenSound => m_openSound;
+
+    [SerializeField]
+    private AudioClip m_closeSound = null;
+    public AudioClip CloseSound => m_closeSound;
+
+    [SerializeField]
+    private AudioClip m_lockedSound = null;
+    public AudioClip LockedSound => m_lockedSound;
+
+    [SerializeField]
+    private CollectibleTemplate m_requiredCollectibleToOpen = null;
+    public CollectibleTemplate RequiredCollectibleToOpen => m_requiredCollectibleToOpen;
+}
