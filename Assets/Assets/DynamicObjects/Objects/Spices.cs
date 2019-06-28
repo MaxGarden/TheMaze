9fece8faa413a878ed00f63a1b6561de9672770b
Mateusz Serkies <mserkies@artifexmundi.com>
2019-06-28 18:04:28 +0200

    Added spices.

---
+++ Assets/Assets/DynamicObjects/Objects/Spices.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Spices : DynamicObject
+{
+    public SpicesTemplate Template { get; private set; }
+
+    public bool AreShown { get; private set; } = true;
+
+    private ISpicesCollisionProvider m_spicesCollisionProvider;
+    private float m_lastCollisionTime = 0.0f;
+
+    public override void Initialize(DynamicObjectTemplate template)
+    {
+        base.Initialize(template);
+
+        Template = (SpicesTemplate)template;
+
+        m_spicesCollisionProvider = Instantiate(Template.CollisionProviderPrefab, transform);
+        m_spicesCollisionProvider.OnCollision += OnSpicesCollision;
+    }
+
+    private void OnDestroy()
+    {
+        if (m_spicesCollisionProvider)
+            m_spicesCollisionProvider.OnCollision -= OnSpicesCollision;
+    }
+
+    public void Show()
+    {
+        AreShown = true;
+    }
+
+    public void Hide()
+    {
+        AreShown = false;
+    }
+
+    private void OnSpicesCollision(GameObject gameObject)
+    {
+        if (!AreShown)
+            return;
+
+        if (Time.time - m_lastCollisionTime < Template.CollisionCooldownInSeconds)
+            return;
+
+        m_lastCollisionTime = Time.time;
+
+        var playerContext = gameObject.GetComponent<PlayerContext>();
+        if (playerContext)
+            OnSpicesCollisionWithPlayer(playerContext);
+    }
+
+    private void OnSpicesCollisionWithPlayer(PlayerContext playerContext)
+    {
+        playerContext.Health.Damage(Template.CollisionDamage);
+
+        var knockbackController = playerContext.KnockbackController;
+        var playerForward = playerContext.transform.forward;
+        knockbackController.Knockback(new Vector3(-playerForward.x, 0.0f, -playerForward.z), Template.KnockbackForce);
+    }
+}
---
+++ Assets/Assets/DynamicObjects/Templates/SpicesTemplate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpicesTemplate", menuName = "Templates/Spices Template")]
+public sealed class SpicesTemplate : DynamicObjectTemplate
+{
+    [Header("Spices")]
+
+    [SerializeField]
+    private ISpicesCollisionProvider m_collisionProviderPrefab = null;
+    public ISpicesCollisionProvider CollisionProviderPrefab => m_collisionProviderPrefab;
+
+    [SerializeField]
+    private float m_collisionDamage = 30.0f;
+    public float CollisionDamage => m_collisionDamage;
+
+    [SerializeField]
+    private float m_knockbackForce = 35.0f;
+    public float KnockbackForce => m_knockbackForce;
+
+    [SerializeField]
+    private float m_collisionCooldownInSeconds = 0.5f;
+    public float CollisionCooldownInSeconds => m_collisionCooldownInSeconds;
+
+    [SerializeField]
+    private float m_timeWhenShownInSeconds = 2.0f;
+    public float TimeWhenShownInSeconds => m_timeWhenShownInSeconds;
+
+    [SerializeField]
+    private float m_timeWhenHiddenInSeconds = 2.0f;
+    public float TimeWhenHiddenInSeconds => m_timeWhenHiddenInSeconds;
+
+    [SerializeField]
+    private AudioClip m_showSound = null;
+    public AudioClip ShowSound => m_showSound;
+
+    [SerializeField]
+    private AudioClip m_hideSound = null;
+    public AudioClip HideSound => m_hideSound;
+}
