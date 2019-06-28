925f6c2f7d32551358889166581acc5461261888
Mateusz Serkies <mserkies@artifexmundi.com>
2019-06-28 18:04:02 +0200

    Added health and knockback controller to player context.

---
+++ Assets/Assets/Player/Health/Health.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public sealed class Health : MonoBehaviour, IPlayerComponent
+{
+    [SerializeField]
+    private float m_maximumHealth = 100.0f;
+    public float MaximumHealth => m_maximumHealth;
+
+    [SerializeField]
+    private AudioClip m_damageSound = null;
+
+    public PlayerContext PlayerContext { get; private set; }
+    public float CurrentHealth { get; private set; }
+
+    private AudioSource m_audioSource;
+
+    public event Action OnHealthChanged;
+
+    private void Awake()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Damage(float damage)
+    {
+        CurrentHealth -= damage;
+
+        m_audioSource.clip = m_damageSound;
+        m_audioSource.Play();
+
+        OnHealthChanged?.Invoke();
+    }
+
+    void IPlayerComponent.OnPlayerContextInitialized(PlayerContext context)
+    {
+        PlayerContext = context;
+        CurrentHealth = MaximumHealth;
+    }
+}
---
+++ Assets/Assets/Player/Knockback/KnockbackController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public sealed class KnockbackController : MonoBehaviour
+{
+    [SerializeField]
+    private float m_mass = 3.0f;
+
+    [SerializeField]
+    private float m_dampingFactor = 5.0f;
+
+    private Vector3 m_impact = Vector3.zero;
+    private CharacterController m_characterController;
+
+    private void Awake()
+    {
+        m_characterController = GetComponent<CharacterController>();
+    }
+
+    public void Knockback(Vector3 direction, float force)
+    {
+        if (direction.y < 0)
+            direction.y *= -1.0f;
+
+        m_impact += direction.normalized * force / m_mass;
+    }
+
+    private void Update()
+    {
+        if (m_impact.magnitude > 0.2f)
+            m_characterController.Move(m_impact * Time.deltaTime);
+
+        m_impact = Vector3.Lerp(m_impact, Vector3.zero, m_dampingFactor * Time.deltaTime);
+    }
+}
