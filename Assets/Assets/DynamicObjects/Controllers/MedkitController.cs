af169da30a8ec6f6881e1a2dbb2138fb11bbf28d
Mateusz Serkies <mserkies@artifexmundi.com>
2019-07-04 20:15:16 +0200

    Added medkit equipment.

---
+++ Assets/Assets/DynamicObjects/Controllers/MedkitController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public sealed class MedkitController : UtilityEquipmentController
+{
+    public new Medkit Equipment { get; private set; }
+    public new MedkitTemplate EquipmentTemplate => Equipment.Template;
+
+    public override void Initialize(Equipment equipment)
+    {
+        base.Initialize(equipment);
+
+        Equipment = (Medkit)equipment;
+    }
+
+    public override void OnUse()
+    {
+        if (!Inventory || Equipment.Durability <= 0.0f)
+            return;
+
+        var health = Inventory.PlayerContext.Health;
+        var missingHealth = health.MaximumHealth - health.CurrentHealth;
+        var availableHealing = Math.Min(Equipment.Durability, EquipmentTemplate.UseCost);
+
+        var healingValue = Math.Min(missingHealth, availableHealing);
+
+        Equipment.Durability -= healingValue;
+        health.Heal(healingValue);
+
+        TryPlayInteractionSound(EquipmentTemplate.RegenerationSound);
+    }
+}
---
+++ Assets/Assets/DynamicObjects/Objects/Medkit.cs
@@ -0,0 +1,11 @@
+﻿public sealed class Medkit : UtilityEquipment
+{
+    public new MedkitTemplate Template { get; private set; }
+
+    protected override void OnInitialize(EquipmentTemplate template)
+    {
+        base.OnInitialize(template);
+
+        Template = (MedkitTemplate)template;
+    }
+}
