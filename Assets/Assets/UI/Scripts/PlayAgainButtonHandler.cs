56db6d6604b583a48021e17f61952c20884d101c
olamusial <olaa12@gmail.com>
2019-09-05 20:44:38 +0200

    restart on a mouse click; changed some sprites

---
+++ Assets/Assets/UI/Scripts/PlayAgainButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgainButtonHandler : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            PlayAgain();
+        }
+    }
+}
