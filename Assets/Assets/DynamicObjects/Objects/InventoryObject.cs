a156459dc775856d4f26f6ba33ca9cccf85874e3
Mateusz Serkies <mserkies@artifexmundi.com>
2019-09-11 21:07:06 +0200

    Fixed auto property backing field serialization issue.

--- Assets/Assets/DynamicObjects/Objects/InventoryObject.cs
+++ Assets/Assets/DynamicObjects/Objects/InventoryObject.cs
@@ -1,11 +1,11 @@
 ﻿public class InventoryObject : InteractiveObject
 {
-    public new InventoryObjectTemplate Template { get; private set; }
+    public new InventoryObjectTemplate Template => (InventoryObjectTemplate)base.Template;
 
     public override void Initialize(DynamicObjectTemplate template)
     {
         base.Initialize(template);
 
-        Template = (InventoryObjectTemplate)template;
+        ValidateTemplate<InventoryObjectTemplate>(Template);
     }
 }
--- Assets/Assets/DynamicObjects/Objects/Medkit.cs
+++ Assets/Assets/DynamicObjects/Objects/Medkit.cs
@@ -2,13 +2,13 @@
 
 public sealed class Medkit : UtilityEquipment
 {
-    public new MedkitTemplate Template { get; private set; }
+    public new MedkitTemplate Template => (MedkitTemplate)base.Template;
 
     protected override void OnInitialize(EquipmentTemplate template)
     {
         base.OnInitialize(template);
 
-        Template = (MedkitTemplate)template;
+        ValidateTemplate<MedkitTemplate>(template);
     }
 
     public bool Heal(Health health)
