867345bbe98f3d952229bcc92a771d77d10a46fd
Jakub Burek <jakubur469@student.polsl.pl>
2019-09-16 18:21:43 +0200

    Added header to medkit template.

--- Assets/Assets/DynamicObjects/Templates/MedkitTemplate.cs
+++ Assets/Assets/DynamicObjects/Templates/MedkitTemplate.cs
@@ -3,6 +3,8 @@
 [CreateAssetMenu(fileName = "MedkitTemplate", menuName = "Templates/Medkit Template")]
 public sealed class MedkitTemplate : UtilityEquipmentTemplate
 {
+    [Header("Medkit")]
+
     [SerializeField]
     private AudioClip m_regenerationSound = null;
     public AudioClip RegenerationSound => m_regenerationSound;
